ae6062a2cae17030ef34f7ee81ce136ae7c0eb14
Anastasios Skandalis <task.skandalis@gmail.com>
2017-02-23 01:03:41 +0000

    Coins, CoinBox, DestructibleBox
    Added Coins, CoinBox, DestructibleBox

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.CompareTag ("Player"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
